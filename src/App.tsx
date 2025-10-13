f264d616efd7cb8ee3a6cf37927b15a62c0b964f
Luuan11 <luancraft55@gmail.com>
2025-10-13 01:17:41 -0300

    :sparkles: feat: projects and education soon

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Hero } from './components/Hero'
 import { Technologies } from './components/Technologies'
 import { Skills } from './components/Skills'
 import { Experience } from './components/Experience'
+import { Projects } from './components/Projects'
+import { Education } from './components/Education'
 import { Footer } from './components/Footer'
 import { ScrollToTop } from './components/ScrollToTop'
 import './App.css'
@@ -17,6 +19,8 @@ function App() {
         <Technologies />
         <Skills />
         <Experience />
+        <Projects />
+        <Education />
         <Footer />
         <ScrollToTop />
       </div>
