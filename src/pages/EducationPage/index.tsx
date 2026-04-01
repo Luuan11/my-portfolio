269ecb9040b182132bac22aeaa74f4a2b9820af2
Luuan11 <luancraft55@gmail.com>
2026-03-31 23:34:17 -0300

    feat: changes on edu and exp

---
+++ src/pages/EducationPage/index.tsx
@@ -0,0 +1,15 @@
+import { Navigation } from '../../components/Navigation'
+import { Education } from '../../components/Education'
+import { Footer } from '../../components/Footer'
+import { ScrollToTop } from '../../components/ScrollToTop'
+
+export function EducationPage() {
+  return (
+    <>
+      <Navigation />
+      <Education />
+      <Footer />
+      <ScrollToTop />
+    </>
+  )
+}
---
+++ src/pages/ExperiencePage/index.tsx
@@ -0,0 +1,15 @@
+import { Navigation } from '../../components/Navigation'
+import { Experience } from '../../components/Experience'
+import { Footer } from '../../components/Footer'
+import { ScrollToTop } from '../../components/ScrollToTop'
+
+export function ExperiencePage() {
+  return (
+    <>
+      <Navigation />
+      <Experience />
+      <Footer />
+      <ScrollToTop />
+    </>
+  )
+}
